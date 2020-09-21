d81f687a5cfecddd39bddc2207c08eab615b2a27
balachander712 <53962993+balachander712@users.noreply.github.com>
2020-09-21 18:34:35 +0530

    added problems

---
+++ Sem3/DS/PS1/6.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+
+    int data;
+    Node* leftChild;
+    Node* rightChild;
+public:
+    Node(int);
+    friend class Tree;
+};
+
+Node:: Node(int value){
+    data = value;
+    leftChild = NULL;
+    rightChild = NULL;
+}
+
+class Tree{
+
+public:
+    Node* root;
+    Tree() { root = NULL; };
+    void insert(Node*&,int);
+    int getNodeCount(Node*&);
+    int getHeight(Node*&);
+};
+
+void Tree:: insert(Node*& node, int value){
+    
+    if(node == NULL){
+        node = new Node(value);
+        return;
+    }
+
+    else if(value < node->data){
+        insert(node->leftChild, value);
+    }
+    else{
+        insert(node->rightChild,value);
+    }
+}
+
+int Tree::getNodeCount(Node*& node){
+    //int count = 1;
+
+    if(node != NULL){
+        int count = 1;
+        count += getNodeCount(node->leftChild) + getNodeCount(node->rightChild);
+        return count;
+    }
+
+    
+}
+
+int Tree::getHeight(Node*& node){
+
+    if(node == NULL) return 0;
+
+    return getHeight(node->leftChild) + getHeight(node->rightChild) + 1;
+}
+
+int main(){
+
+    Tree tree;
+    int MONK[] = {1,4,6,12,20,14,54,32};
+    int len = (sizeof(MONK)/sizeof(MONK[0]));
+    
+    for(int i = 0; i < len; i++){
+        tree.insert(tree.root,MONK[i]);
+    }
+
+    cout << tree.getHeight(tree.root) << endl; 
+
+    return 0;
+}
--- Sem3/DS/PS2/1.cpp
+++ Sem3/DS/PS2/1.cpp
@@ -1,239 +1,174 @@
 #include <iostream>
 using namespace std;
 
+
 class Node{
 
     int data;
+    int height;
     Node* leftChild;
     Node* rightChild;
-    int height;
 public:
-    Node();
     Node(int);
-    void insert(int);
-    int getNodeHeight(Node);
-    int getBalanceFactor(Node);
-    void LLRotation(Node);
-    void LRRotation(Node);
-    void RRRotation(Node);
-    void RLRotation(Node);
-    bool hasLeftChild();
-    bool hasRightChild();
-    void traverseInorder();
+    friend class Tree;
 };
 
-Node:: Node(){
+Node:: Node(int value){
+    data = value;
+    height = 0;
     leftChild = NULL;
     rightChild = NULL;
-    height = 0;
 }
 
-Node:: Node(int value){
-    this->data = value;
-}
 
-void Node:: insert(int value){
-    if(data < value){
-        if(this->leftChild == NULL){
-            leftChild = new Node(value);
-        }
-        else
-        {
-            leftChild->insert(value);
-        }
+class Tree{
+
+public:
+    Node* root;
+    Tree() { root = NULL; };
+    void insert(Node*&,int);
+    void traverseInOrder(Node*&);
+    void LLRotation(Node*&);
+    void LRRotation(Node*&);
+    void RRRotation(Node*&);
+    void RLRotation(Node*&);
+    int getNodeHeight(Node*&);
+    int getBalanceFactor(Node*&);
+
+};
+
+void Tree:: insert(Node*& node, int value){
+    
+    if(node == NULL){
+        node = new Node(value);
+        return;
+    }
+
+    else if(value < node->data){
+        insert(node->leftChild, value);
+    }
+    else{
+        insert(node->rightChild,value);
+    }
+
+    node->height = getNodeHeight(node);
+
+    if(getBalanceFactor(node) == 2  && getBalanceFactor(node->leftChild) == 1){
+        //perform LL rotation
+        LLRotation(node);
+        traverseInOrder(root);
+    }
+    else if(getBalanceFactor(node) == 2 && getBalanceFactor(node->leftChild) == -1){
+        //perform LR rotation
+        LRRotation(node);
+        traverseInOrder(root);
+    }
+    else if(getBalanceFactor(node) == -2 && getBalanceFactor(node->rightChild) == -1){
+        //perform RR rotation
+        RRRotation(node);
+        traverseInOrder(root);
+    }
+    else if(getBalanceFactor(node) == -2 && getBalanceFactor(node->rightChild) == 1){
+        // perform RL rotation
+        RLRotation(node);
+        traverseInOrder(root);
+    }
+
         
-    }
-    else
-    {
-        if(this->rightChild == NULL){
-            rightChild = new Node(value);
-        }
-        else
-        {
-            rightChild->insert(value);
-        }
-    }
-
-    height = getNodeHeight(*this);
-
-    if(getBalanceFactor(*this) == 2 && value < this->leftChild->data)
-        LLRotation(*this);
-    if(getBalanceFactor(*this) == 2 && value > this->leftChild->data)
-        LRRotation(*this);
-    if(getBalanceFactor(*this) == -2 && value > this->rightChild->data)
-        RRRotation(*this);
-    if(getBalanceFactor(*this) == -2 && value < this->rightChild->data)
-        RLRotation(*this);
-
-    
-    traverseInorder();
 }
 
-void Node:: LLRotation(Node node){
-    //implement rotations
-    Node* left = node.leftChild;
+void Tree::LLRotation(Node*& node){
+
+    Node* left = node->leftChild;
     Node* leftRight = left->rightChild;
 
-    left->rightChild = &node;
-    node.leftChild = leftRight;
+    left->rightChild = node;
+    node->leftChild = leftRight;
 
-    node.height = getNodeHeight(node);
-    left->height = getNodeHeight(*left);
+    node->height = getNodeHeight(node);
+    left->height = getNodeHeight(left);
 
-    AVLTree temp;
-    if(temp.root == &node){
-        temp.root = left;
-    }
-    
+    if(root == node) root = left;
 }
 
-void Node:: LRRotation(Node node){
-    //implement rotations
-    Node* left = node.leftChild;
+void Tree::LRRotation(Node*& node){
+
+    Node* left = node->leftChild;
     Node* leftRight = left->rightChild;
-    Node* leftRightRight = leftRight->rightChild;
     Node* leftRightLeft = leftRight->leftChild;
+    Node* leftRightRight = leftRightLeft->rightChild;
 
-    leftRight->rightChild = &node;
+    leftRight->rightChild = node;
     leftRight->leftChild = left;
-    left->rightChild = leftRightLeft;
-    node.rightChild = leftRightRight;
+    node->rightChild = leftRightRight;
+    left->leftChild = leftRightLeft;
 
-    node.height = getNodeHeight(node);
-    left->height = getNodeHeight(*left);
-    leftRight->height = getNodeHeight(*leftRight);
+    leftRight->height = getNodeHeight(leftRight);
+    node->height = getNodeHeight(node);
+    left->height = getNodeHeight(left);
 
-    AVLTree temp;
-    if(temp.root == &node){
-        temp.root == leftRight;
-    }
-    
+    if(root == node) root = leftRight;
 }
 
-void Node:: RLRotation(Node node){
-    //implement rotations
-    
-    Node* right = node.rightChild;
+
+void Tree::RRRotation(Node*& node){
+
+    Node* right = node->rightChild;
+    Node* rightLeft = right->leftChild;
+
+    right->leftChild = node;
+    node->rightChild = rightLeft;
+
+    right->height = getNodeHeight(right);
+    node->height = getNodeHeight(node);
+
+    if(root == node) root = right;
+}
+
+void Tree::RRRotation(Node*& node){
+
+    Node* right = node->rightChild;
     Node* rightLeft = right->leftChild;
     Node* rightLeftLeft = rightLeft->leftChild;
-    Node* rigthLeftRight = rightLeft->rightChild;
+    Node* rightLeftRight = rightLeft->rightChild;
 
+    rightLeft->leftChild = node;
     rightLeft->rightChild = right;
-    rightLeft->leftChild = &node;
-    right->leftChild = rigthLeftRight;
-    node.rightChild = rightLeftLeft;
+    node->rightChild = rightLeftLeft;
+    right->leftChild = rightLeftRight;
 
-    rightLeft->height = getNodeHeight(*rightLeft);
-    node.height = getNodeHeight(node);
-    right->height = getNodeHeight(*right);
+    rightLeft->height = getNodeHeight(rightLeft);
+    node->height = getNodeHeight(node);
+    right->height = getNodeHeight(right);
 
-    AVLTree temp;
-    if(temp.root == &node){
-        temp.root = rightLeft;
-    }
+    if(root == node) root = rightLeft;
 }
 
-void Node:: RRRotation(Node node){
-    //implement rotations
-    Node* right = node.rightChild;
-    Node* rightLeft = right->leftChild;
+int Tree:: getNodeHeight(Node*& node){
 
-    right->leftChild = &node;
-    node.rightChild = rightLeft;
-
-    node.height = getNodeHeight(node);
-    right->height = getNodeHeight(*right);
-
-    AVLTree temp;
-    if(temp.root == &node){
-        temp.root = right;
-    }
-}
-
-void Node:: traverseInorder(){
-    if(this->leftChild != NULL){
-        this->traverseInorder();
-    }
-
-    cout << this->data << " " ;
-
-    if(this->rightChild != NULL){
-        this->traverseInorder();
-    }
-}
-
-int Node:: getNodeHeight(Node node){
-    
     int left,right;
-    left = node.hasLeftChild() ? node.height : 0;
-    right = node.hasRightChild() ? node.height : 0;
+
+    left = node->leftChild == NULL ? 0 : node->leftChild->height;
+    right = node->rightChild == NULL ? 0 : node->rightChild->height;
 
     return left > right ? left + 1 : right + 1;
 }
 
-int Node:: getBalanceFactor(Node node){
-
+int Tree:: getBalanceFactor(Node*& node){
     int left,right;
 
-    left = node.hasLeftChild() ? node.leftChild->height : 0;
-    right = node.hasRightChild() ? node.rightChild->height : 0;
+    left = node->leftChild == NULL ? 0 : node->leftChild->height;
+    right = node->rightChild == NULL ? 0 : node->rightChild->height;
 
     return left - right;
-
 }
 
-
-
-bool Node:: hasLeftChild(){
-    if(this->leftChild != NULL){
-        return true;
+void Tree:: traverseInOrder(Node*& node){
+    if(node != NULL){
+        traverseInOrder(node->leftChild);
+        cout << node->data << " ";
+        traverseInOrder(node->rightChild);
     }
-
-    return false;
-}
-
-bool Node:: hasRightChild(){
-    if(this->rightChild !=NULL){
-        return true;
-    }
-
-    return false;
-}
-
-class AVLTree{
-
-public:
-
-    static Node* root;
-    AVLTree();
-    void insert(int);
-    void traverseInorder();
-};
-
-AVLTree:: AVLTree(){
-    root = NULL;
-}
-
-void AVLTree:: insert(int value){
-    if(root == NULL){
-        root = new Node(value);
-    }
-    else
-    {
-        root->insert(value);
-    }
-    
-}
-
-void AVLTree:: traverseInorder(){
-    if(root != NULL){
-        root->traverseInorder();
-    }
-    else
-    {
-        cout << "EMPTY TREE!!!" << endl;
-    }
-    
 }
 
 int main(){
