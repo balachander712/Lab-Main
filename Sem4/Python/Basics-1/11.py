1e13378c1e0eacc2d1e1180cb9d73aa798f72d08
balachander712 <53962993+balachander712@users.noreply.github.com>
2021-01-10 20:55:41 +0530

    edit 11

--- Sem4/Python/Basics-1/11.py
+++ Sem4/Python/Basics-1/11.py
@@ -1,18 +1,18 @@
-import math
-
-def kaprekar(num):
-    sq = num * num
-    digits = int(math.log10(sq))+1
-    n = digits / 2
-    if digits % 2 == 1:
-        n += 1
-    right = sq % (10 ** n)
-    left = sq / (10 ** n)
-
-    if right + left == num:
+def is_keprekar(num):
+    square = num * num
+    length = len(str(num))
+    square_str = str(square)
+    right = int(square_str[-1*length:])
+    left = int(square_str[:-1*length])
+    if right+left == num:
         return True
     else:
         return False
 
-if __name__ == '__main__':
-    print(kaprekar(9))
+
+if __name__  == "__main__":
+    num = int(input("Please enter the number: "))
+    if is_keprekar(num):
+        print("This is a keprekar number")
+    else:
+        print("This is not a keprekar number")
